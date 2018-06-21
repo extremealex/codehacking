22585d6f1ff996cf37f99a9885ad79942773c088
Alex <alex@dreamyfox.com>
2018-06-21 12:05:15 +0800

    Upload photos and encrypt password added

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
@@ -45,7 +46,18 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        User::create($request->all());
+        $input = $request->all();
+
+        if ($request->hasFile('photo_id')) {
+            $file = $request->file('photo_id');
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['file->$name']);
+            $input['photo_id'] = $photo->id;
+        }
+
+        $input['password'] = bcrypt($input['password']);
+        User::create($input);
 
         return redirect(url('admin/users'));
     }
